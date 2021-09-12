60cec642677250b61c05058658ea4991c2300662
Vlad Nikitin <nikitinv91@gmail.com>
2021-09-12 14:15:28 +0300

    preparing models

--- portfolio_website/main/models.py
+++ portfolio_website/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Experience(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Company title')
+    position = models.CharField(max_length=150, verbose_name='Occupation')
+    period = models.CharField(
+        max_length=50, verbose_name='Period of employement')
+    location = models.CharField(
+        max_length=100, verbose_name='Company location', blank=True)
+    description = models.TextField(verbose_name='Overview of the experience')
+    logo = models.ImageField(('Logo'), upload_to='experience/', blank=True)
